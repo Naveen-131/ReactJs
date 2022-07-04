12c34e03233d10cd45e471ecf6b2b3033fd1b62b
Naveen-131 <67405994+Naveen-131@users.noreply.github.com>
2022-07-04 12:50:08 +0530

    Add files via upload

---
+++ Random User/src/MyCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { FaEnvelope, FaMapMarkedAlt, FaPhone } from "react-icons/fa"
+
+const MyCard = ({ details }) => {
+    
+    return (
+
+        <Card>
+            <CardBody className="text-center">
+                <img
+                    height="150"
+                    width="150"
+                    className="rounded-circle img-thumbnail border-danger"
+                    src={details.picture?.large}
+                    alt=" "
+                />
+                <CardTitle className="text-primary">
+                    <h1>
+                        <span>{details.name?.title} </span>
+                        <span>{details.name?.first}</span>
+                        <span>{details.name?.last}</span>
+
+                    </h1>
+                </CardTitle>
+                <CardText>
+                    <FaMapMarkedAlt /> 
+                    {details.location?.city}
+                    <p>{details.phone}</p>
+                    <p>{details.dob?.age}</p>
+                    <p>{ details.email}</p>
+                    
+                </CardText>
+        
+                
+            </CardBody>
+        </Card>
+    );
+}
+ 
+export default MyCard;
